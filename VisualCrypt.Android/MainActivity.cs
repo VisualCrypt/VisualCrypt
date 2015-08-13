27b1b040ad4dc2346f805c9bd61ba176c8aba346
Claus Ehrenberg <ce@aubergemediale.com>
2015-08-13 02:04:36 +0200

    Add Android and iOS projects

---
+++ VisualCrypt.Android/MainActivity.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using VisualCrypt.Cryptography.Net.VisualCrypt2.Implementations;
+using VisualCrypt.Cryptography.Portable.VisualCrypt2.Implementations;
+
+namespace VisualCrypt.Android
+{
+    [Activity(Label = "VisualCrypt.Android", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        int count = 1;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            // Get our button from the layout resource,
+            // and attach an event to it
+            Button button = FindViewById<Button>(Resource.Id.MyButton);
+
+            button.Click += delegate { button.Text = string.Format("{0} clicks!", count++); };
+
+            var api = new VisualCrypt2API(new Platform_Net4());
+            button.Text = api.GenerateRandomPassword().Result;
+        }
+    }
+}
+
---
+++ VisualCrypt.iOS/Main.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UIKit;
+using VisualCrypt.Cryptography.Portable.VisualCrypt2.Implementations;
+
+namespace VisualCrypt.iOS
+{
+    public class Application
+    {
+        // This is the main entry point of the application.
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello");
+            // if you want to use a different Application Delegate class from "AppDelegate"
+            // you can specify it here.
+            var api = new VisualCrypt2API(null);
+            UIApplication.Main(args, null, "AppDelegate");
+        }
+    }
+}
